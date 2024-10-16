3905fc80845146e02abed00fdb050d1c2d0f20c0
Julia Mertz <info@juliamertz.dev>
2024-10-16 03:47:13 +0200

    update formatter

--- packages/dtsfmt.nix
+++ packages/dtsfmt.nix
@@ -8,17 +8,11 @@
 rustPlatform.buildRustPackage rec {
   pname = "dtsfmt";
   version = "v0.3.1";
-  # src = inputs.dtsfmt;
+  meta.mainProgram = pname;
   src = fetchgit {
     url = "https://github.com/juliamertz/${pname}.git";
-    rev = "a80ab3e56d52fda659bcd3860750ca7ec553944c";
-    hash = "sha256-a641ypgBy2mQU9wZyCl7nQEwplarrnFvg7vjXKbkd78=";
+    rev = "b47a3720002ca06d657c7147f7dc56ca7abd68eb";
+    hash = "sha256-iepKJVZmCPo6TKoy1+kI41N7tDy+Ozf2SjW9bkcVmLo=";
   };
-  # src = runCommand "src" { } ''
-  #   mkdir -p $out/tree-sitter-devicetree
-  #   cp -r ${inputs.dtsfmt}/* $out/
-  #   cp -r ${inputs.treesitter-devicetree}/* $out/tree-sitter-devicetree
-  # '';
-  meta.mainProgram = "dtsfmt";
   cargoHash = "sha256-5fMoMaDHAAhfaTEdFwMqzD6N2zheh+AIjgCmaZZex84=";
 }
